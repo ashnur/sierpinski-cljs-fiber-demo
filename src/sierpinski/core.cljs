96b1b33b00b789d96eddf036dcd528172bb6a790
Aron G <ashnur@gmail.com>
2020-05-08 08:11:17 +0100

    fixes and empirics

--- src/sierpinski/core.cljs
+++ src/sierpinski/core.cljs
@@ -1,27 +1,38 @@
 (ns sierpinski.core
   (:require [helix.core :refer [$]]
+            ["react" :as react]
             ["react-dom" :as rdom]
             [sierpinski.experimental :as experimental]))
 
 (def start (.now js/Date))
 
 (def root (atom nil))
+(def container (atom nil))
+
+(defn- clear-children [] 
+  (when-let [root-node @root] 
+    (.unmount root-node)
+    (when-let [container-node @container]
+      (doseq [ch (.-children container-node)]
+        (.remove ch)))))
+                                
 
 (defn step
   []
-  (.render @root ($ experimental/demo {:elapsed (- (.now js/Date) start)}))
+  (when-let [root-node @root] (.render root-node ($ (.-StrictMode react) ($ experimental/demo {:elapsed (- (.now js/Date) start)}))))
   (.requestAnimationFrame js/window step)) 
 
 
 (defn ^:dev/after-load render
   ; FIXME  start this after reload of code not page?
-  [])
+  []
+  (clear-children)
+  (step))
 
 (defn ^:export main
   ;; FIXME this definitely runs at startup
   "Run application startup logic."
   []
-  (when-let [container (.getElementById js/document "demo")]
-    (when-not (some? @root)
-      (reset! root (rdom/createRoot container))))
+  (reset! container (.getElementById js/document "demo"))
+  (reset! root (.createRoot rdom @container))
   (step)) 
--- src/sierpinski/experimental.cljs
+++ src/sierpinski/experimental.cljs
@@ -18,8 +18,16 @@
                :font-size "1.3rem"
                :textAlign "center"
                :cursor "pointer"})
+
+(defn- cyc [x] (mod x 10))
+
+(defn- schedule-setter
+  [setter value] 
+  (fn [] (.requestAnimationFrame js/window #(setter value))))
+
 (defnc Dot
   [{:keys [size idx x y text]}]
+  {:wrap [memo]}
   (let [s (* 1.3 size)
         [hover set-hover] (hooks/use-state false)]
     ($ "div" {:style (merge dotStyle {:width (str s "px")
@@ -29,8 +37,8 @@
                                       :borderRadius (str (/ s 2) "px")
                                       :lineHeight (str s "px")
                                       :background (if hover "#ff0" (:background dotStyle))})
-              :onMouseEnter #(set-hover true)
-              :onMouseLeave #(set-hover false)}
+              :onMouseEnter (schedule-setter set-hover true)
+              :onMouseLeave (schedule-setter set-hover false)}
       (if hover "X" text))))
 
 (defnc SierpinskiTriangle
@@ -42,27 +50,38 @@
             :size targetSize
             :text children})
     (let [size (/ s 2) 
-          e (+ 0.3 (.now js/performance))]
-      ; (while (< e (.now js/performance)))  
+          e (+ 0.8 (.now js/performance))]
+      (while (< e (.now js/performance)))  
       ; (prn x y s)
       (<>
-        ($ SierpinskiTriangle {:x x :y (- y (/ size 2)) :s size}
-          (<> children))
-        ($ SierpinskiTriangle {:x (- x size) :y (+ y (/ size 2)) :s size}
-          (<> children))
-        ($ SierpinskiTriangle {:x (+ x size) :y (+ y (/ size 2)) :s size}
-          (<> children))))))
-  
+        ($ SierpinskiTriangle {:key "top" :x x :y (- y (/ size 2)) :s size}
+          children)
+        ($ SierpinskiTriangle {:key "left" :x (- x size) :y (+ y (/ size 2)) :s size}
+          children)
+        ($ SierpinskiTriangle {:key "right" :x (+ x size) :y (+ y (/ size 2)) :s size}
+          children)))))
+
 
 (defnc demo
   [{:keys [elapsed]}]
-  (let [seconds (mod (/ elapsed 1000) 10) 
-        scale (+ 1 (/ (if (> seconds 5) 
-                       (- 10 seconds)
-                       seconds)
+  {:wrap [memo]
+   :helix/features {:check-invalid-hooks-usage true}}
+  (let [[seconds set-seconds] (hooks/use-state 0)
+        t (cyc ( / elapsed 1000)) 
+        scale (+ 1 (/ (if (> t 5) 
+                       (- 10 t)
+                       t)
                      10))
-        transform {:transform (str "scaleX(" (/ scale 2.1) ") scaleY(0.7) translateZ(0.1px)")}
+        transform {:transform (str "scaleX(" (/ scale 2.2) ") scaleY(0.7) translateZ(0.1px)")}
         lowPriChildren false]
+
+    (hooks/use-effect
+      [set-seconds]
+      (let [interval-id (.setInterval js/window (schedule-setter set-seconds #(-> % inc cyc)) 1000)]
+        #(.clearInterval js/window interval-id)))
+
+    ; (prn seconds)
+
     ($ "div" {:style (merge container-style transform)}
       ($ "div" {:hidden lowPriChildren} 
         ($ SierpinskiTriangle {:x 0 :y 0 :s 1000}
